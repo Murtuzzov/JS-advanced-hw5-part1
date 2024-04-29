dc17356c1f78ce88cd89fbb0f37456d027d639ee
murtuzzov <murtuzzov>
2024-04-29 23:34:22 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,96 @@
+//ЗАДАЧА No1
+// const links = document.querySelectorAll(".links-list a");
+
+// links.forEach((link) => {
+//   if (link.href.startsWith("https://")) {
+//     link.style.borderBottom = "1px dashed black";
+//   }
+// });
+
+//_______________________________________________________________________________
+
+//ЗАДАЧА No2
+
+// const localDiskC = document.querySelector(".local-diskC");
+// const diskC = document.querySelector(".data");
+// const diskCSecond = document.querySelector(".data2");
+// const diskCThird = document.querySelector(".data3");
+
+// const diskCList = document.querySelector(".C");
+// localDiskC.addEventListener("click", () => {
+//   diskCList.classList.toggle("hiddenC");
+// });
+
+// localDiskC.addEventListener("mouseover", () => {
+//   localDiskC.style.fontWeight = "bold";
+// });
+
+// localDiskC.addEventListener("mouseout", () => {
+//   localDiskC.style.fontWeight = "normal";
+// });
+
+// diskC.addEventListener("mouseover", () => {
+//   diskC.style.fontWeight = "bold";
+// });
+
+// diskC.addEventListener("mouseout", () => {
+//   diskC.style.fontWeight = "normal";
+// });
+
+// diskCSecond.addEventListener("mouseover", () => {
+//   diskCSecond.style.fontWeight = "bold";
+// });
+
+// diskCSecond.addEventListener("mouseout", () => {
+//   diskCSecond.style.fontWeight = "normal";
+// });
+
+// diskCThird.addEventListener("mouseover", () => {
+//   diskCThird.style.fontWeight = "bold";
+// });
+
+// diskCThird.addEventListener("mouseout", () => {
+//   diskCThird.style.fontWeight = "normal";
+// });
+
+// const localDiskD = document.querySelector(".local-diskD");
+// const diskD = document.querySelector(".data4");
+// const diskDSecond = document.querySelector(".data5");
+// const diskDThird = document.querySelector(".data6");
+// const diskDList = document.querySelector(".D");
+
+// localDiskD.addEventListener("click", () => {
+//   diskDList.classList.toggle("hiddenD");
+// });
+
+// localDiskD.addEventListener("mouseover", () => {
+//   localDiskD.style.fontWeight = "bold";
+// });
+
+// localDiskD.addEventListener("mouseout", () => {
+//   localDiskD.style.fontWeight = "normal";
+// });
+
+// diskD.addEventListener("mouseover", () => {
+//   diskD.style.fontWeight = "bold";
+// });
+
+// diskD.addEventListener("mouseout", () => {
+//   diskD.style.fontWeight = "normal";
+// });
+
+// diskDSecond.addEventListener("mouseover", () => {
+//   diskDSecond.style.fontWeight = "bold";
+// });
+
+// diskDSecond.addEventListener("mouseout", () => {
+//   diskDSecond.style.fontWeight = "normal";
+// });
+
+// diskDThird.addEventListener("mouseover", () => {
+//   diskDThird.style.fontWeight = "bold";
+// });
+
+// diskDThird.addEventListener("mouseout", () => {
+//   diskDThird.style.fontWeight = "normal";
+// });
